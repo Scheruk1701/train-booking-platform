4f18736b71e253291ef991fe0edb460ee7a63cd1
Sudhish Cherukuri <sudhishcherukuri@Sudhishs-MacBook-Air.local>
2026-02-16 15:48:39 -0500

    adding intial files entities and services

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,17 @@
+package ticket.booking.entities;
+import java.util.*;
+
+public class Ticket {
+
+    private String ticketId;
+
+    private String userId;
+
+    private String source;
+
+    private String destination;
+
+    private Date dateOfTravel;
+
+    private Train train;
+}
